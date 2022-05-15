f8bd860cdf28b1ff0937127f5c3e0267a81cde75
NeshGogo <rafaelaguero10@hotmail.com>
2022-05-15 11:45:32 -0400

    init and input

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent implements OnInit {
+
+  @Input() url = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
