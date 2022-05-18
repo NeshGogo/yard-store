db2228e749e092e1a7a360d8e25f2770bea78095
NeshGogo <rafaelaguero10@hotmail.com>
2022-05-17 23:18:02 -0400

    products component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +9,6 @@ export class AppComponent {
   title = 'my-store';
   imgUrl = '';
   toggle = true;
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'El mejor juguete',
-      image: './assets/img/toy.jpg',
-      price: 565
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      image: './assets/img/bike.jpg',
-      price: 356
-    },
-    {
-      id: '3',
-      image: './assets/img/album.jpg',
-      name: 'Collección de albumnes',
-      price: 34
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      image: './assets/img/books.jpg',
-      price: 23
-    },
-  ]
 
   onloaded(url: string){
     console.log('From father', url);
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'El mejor juguete',
+      image: './assets/img/toy.jpg',
+      price: 565
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      image: './assets/img/bike.jpg',
+      price: 356
+    },
+    {
+      id: '3',
+      image: './assets/img/album.jpg',
+      name: 'Collección de albumnes',
+      price: 34
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      image: './assets/img/books.jpg',
+      price: 23
+    },
+  ]
 
   constructor() { }
 
