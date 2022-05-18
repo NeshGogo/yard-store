6c0cc0ed07c1f647d70b4f76e2f89938f36d94ce
NeshGogo <rafaelaguero10@hotmail.com>
2022-05-17 22:13:59 -0400

    product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-store';
   imgUrl = '';
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'El mejor juguete',
+      image: './assets/img/toy.jpg',
+      price: 565
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      image: './assets/img/bike.jpg',
+      price: 356
+    },
+    {
+      id: '3',
+      image: './assets/img/album.jpg',
+      name: 'Collección de albumnes',
+      price: 34
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      image: './assets/img/books.jpg',
+      price: 23
+    },
+  ]
 
   onloaded(url: string){
     console.log('From father', url);
