670384c49ef39dc282882373bfb6d7fc19534635
NeshGogo <rafaelaguero10@hotmail.com>
2022-05-22 12:21:03 -0400

    adding angular lint

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -15,39 +15,19 @@ import {
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss'],
 })
-export class ImgComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit {
+export class ImgComponent {
   url: string = '';
   // we can customise the input name like this
+  // eslint-disable-next-line @angular-eslint/no-input-rename
   @Input('url') set setUrl(value: string) {
     this.url = value;
-    console.log('only url changed', this.url);
     //some code after change.
   }
   @Input() alt = '';
-  @Output() onLoadedTest = new EventEmitter<string>();
+  @Output() loaded = new EventEmitter<string>();
   defaultUrl = './assets/img/default.png';
-  counter = 0;
-  counterFn: undefined | number;
 
   constructor() {
-    console.log('constructor');
-  }
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('change', changes);
-  }
-  ngOnDestroy(): void {
-    console.log('img was destroy');
-    window.clearInterval(this.counterFn);
-  }
-
-  ngOnInit(): void {
-    // this.counterFn = window.setInterval(() => {
-    //   this.counter++;
-    //   console.log('run counter');
-    // }, 1000);
   }
 
   imgError(): void {
@@ -55,7 +35,6 @@ export class ImgComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit
   }
 
   isLoaded(): void {
-    console.log('from child');
-    this.onLoadedTest.emit(this.url);
+    this.loaded.emit(this.url);
   }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../models/product.model';
 
 @Component({
@@ -6,7 +6,7 @@ import { Product } from '../../models/product.model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
   @Output() addedProduct = new EventEmitter<Product>();
 
   @Input() product: Product = {
@@ -18,9 +18,6 @@ export class ProductComponent implements OnInit {
     description: '',
   }
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   addProduct(): void {
     this.addedProduct.emit(this.product);
