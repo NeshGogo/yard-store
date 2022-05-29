d53b7a07eee051108d3bdf09849fa2d37e92ffb0
NeshGogo <rafaelaguero10@hotmail.com>
2022-05-29 13:39:03 -0400

    show all categories

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/product.model';
+import { CategoriesService } from 'src/app/services/categories.service';
 import { StoreService } from 'src/app/services/store.service';
 
 @Component({
@@ -10,8 +12,12 @@ export class NavComponent implements OnInit {
   showMenu = false;
   counter = 0;
   userEmail = '';
+  categories: Category[] = [];
 
-  constructor(private storeService: StoreService) {}
+  constructor(
+    private storeService: StoreService,
+    private categoriesService: CategoriesService
+  ) {}
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe((products) => {
@@ -20,6 +26,11 @@ export class NavComponent implements OnInit {
     this.storeService.user$.subscribe((user) => {
       this.userEmail = user.email;
     });
+    this.categoriesService
+      .get()
+      .subscribe((categories) => {
+        this.categories = categories;
+      });
   }
 
   toggleMenu(): void {
