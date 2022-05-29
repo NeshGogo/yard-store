da58318913e20c65e0d6540b211a8e20b30be18a
NeshGogo <rafaelaguero10@hotmail.com>
2022-05-29 12:25:00 -0400

    products by categories pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,10 @@ const routes: Routes = [
     component: CategoryComponent,
   },
   {
+    path: 'categories/:id',
+    component: CategoryComponent,
+  },
+  {
     path: 'cart',
     component: MycartComponent,
   },
--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ProductsService } from 'src/app/services/products.service';
+import {switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent implements OnInit {
+  products: Product[] = [];
+  categoryId: string | null = null;
+  limit = 10;
+  offset = 0;
 
-  constructor() { }
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        tap((params: ParamMap) => {
+          this.categoryId = params.get('id');
+        }),
+        switchMap(() => {
+          if (this.categoryId) {
+            return this.productsService.getByCategory(this.categoryId, this.limit, this.offset);
+          }
+          return [];
+        })
+      )
+      .subscribe((products) => {
+        this.products = products;
+      });
   }
-
 }
