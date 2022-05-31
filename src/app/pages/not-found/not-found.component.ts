48e46b431716e014ffa5dc2d0b1c34efd9f62f41
NeshGogo <rafaelaguero10@hotmail.com>
2022-05-30 21:36:42 -0400

    not found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -2,14 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  template: `<div>
+    <img src="https://media.giphy.com/media/A9EcBzd6t8DZe/giphy.gif" />
+  </div>`,
+  styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NotFoundComponent {}
