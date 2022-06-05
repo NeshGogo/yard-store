563a3d7ffc15180b739f47c923b2270751f4f6d1
NeshGogo <rafaelaguero10@hotmail.com>
2022-06-05 16:17:31 -0400

    canDeactive guard

--- src/app/webSite/web-site-routing.module.ts
+++ src/app/webSite/web-site-routing.module.ts
@@ -10,6 +10,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDatailComponent } from './pages/product-datail/product-datail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { AuthGuard } from '../guards/auth.guard';
+import { ExitGuard } from '../guards/exit.guard';
 
 const routes: Routes = [
   {
@@ -47,6 +48,7 @@ const routes: Routes = [
       {
         path: 'register',
         component: RegisterComponent,
+        canDeactivate:[ExitGuard]
       },
       {
         path: 'recovery',
